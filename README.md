d2583feb8f27ea5a720b98f35d9e715cf8d2b264
Ralph Depping <github@depping.me>
2023-07-11 19:25:28 +0100

    add export to csv feature

--- README.md
+++ README.md
@@ -4,15 +4,13 @@ check mp3 files for a set of expected id3 tags
 Install dependencies
 `poetry install` or `pip install mutagen`
 
-Edit `mp3_id3_check.py` to update for the list of expected tags
-
-
+Edit `mp3_id3_check.py` to update for the list of expected tags, export headers to csv etc.
 
 Help
 
 ```commandline
 ❯ poetry run python mp3_id3_check.py --help
-usage: mp3_id3_check.py [-h] [-s] [-c] [FOLDER_PATH]
+usage: mp3_id3_check.py [-h] [-s] [-c] [-x] [FOLDER_PATH]
 
 Check MP3 files for expected ID3 tags.
 
@@ -23,6 +21,7 @@ options:
   -h, --help     show this help message and exit
   -s, --summary  include a summary
   -c, --correct  (future) automatically correct missing tags where possible
+  -x, --export   Export csv summary
 ```
 
 Example run with summary
@@ -30,38 +29,49 @@ Example run with summary
 ```commandline
 ❯  poetry run python mp3_id3_check.py /Users/user/mp3-folder -s
 
-Total files checked: 4
-Compliant files: 4
-Non-compliant files: 0
+Total files checked: 6, Compliant: 6 Non-compliant: 0
 
 Tag Summary:
 
-=== albumartist:
-Carrigaline Baptist Church (4 file(s))
-
-=== date:
-2022 (1 file(s))
-2019 (1 file(s))
-2023 (1 file(s))
-2020 (1 file(s))
-
 === album:
 Galatians - Gospel of Grace (1 file(s))
-Genesis - The Promised Seed (1 file(s))
-Christmas (1 file(s))
+Carrigaline Baptist Church (1 file(s))
+Christmas (2 file(s))
 Revelation - Victory Through Suffering (1 file(s))
+Genesis - The Promised Seed (1 file(s))
+
+=== albumartist:
+Carrigaline Baptist Church (6 file(s))
+
+=== artist:
+Joe Smart (1 file(s))
+Barry Reader (2 file(s))
+Henry Bloggs (3 file(s))
 
 === title:
-Jesus Comfort For All People  - Luke Ch2:22-40 (1 file(s))
 Revelation Ch22v6-21 - Jesus Is Coming (1 file(s))
+Jesus Comfort For All People  - Luke Ch2:22-40 (2 file(s))
+Daniel: Dreams Do Come True (1 file(s))
 Genesis Ch26v1-33 - Relentlessly Faithful (1 file(s))
 Grace Restoration - Galatians 6:1-6 (1 file(s))
 
-=== artist:
-Joe Bloggs (1 file(s))
-Jonny Supreme (2 file(s))
-Sam Roberts (1 file(s))
+=== date:
+2020 (1 file(s))
+2019 (2 file(s))
+2022 (1 file(s))
+2014 (1 file(s))
+2023 (1 file(s))
+```
 
-=== genre:
-sermon (4 file(s))
+Example run with export
+
+```commandline
+❯  poetry run python mp3_id3_check.py /Users/user/mp3-folder -x
+Skipping export of tag genre from Genesis Ch26v1-33 - Relentlessly Faithful.mp3
+Skipping export of tag genre from danielCh2_dreamsDoComeTrue_20140914.mp3
+Skipping export of tag genre from jesusComfortForAllPeople_20191229.mp3
+Skipping export of tag genre from galatiansCh6_graceRestoration_20200705.mp3
+Skipping export of tag genre from jesusComfortForAllPeople_20191229 (1).mp3
+Skipping export of tag genre from revelation22_JesusIsComing_20221218.mp3
+Exported 6 to sermons.csv
 ```
--- mp3_id3_check.py
+++ mp3_id3_check.py
@@ -3,21 +3,30 @@
 import os
 import argparse
 from mutagen.easyid3 import EasyID3
-
-# boostrapped via chatgpt - use beware
+import csv
+from pathlib import Path
 
 # Define the expected ID3 tags
-expected_tags = {
+expected_tags = [
     'album',
     'albumartist',
     'artist',
     'title',
     'date',
-    'genre',
-}
+]
+
+# List of headers for exported csv (sermon audio specific)
+csv_headers = [
+    'series',
+    'title',
+    'preacher',
+    'year',
+    'filepath',
+    'filename',
+]
 
 
-def check_mp3_files(folder_path, summary):
+def check_mp3_files_have_tags(folder_path, summary):
     # Initialize counters
     total_files = 0
     compliant_files = 0
@@ -49,15 +58,13 @@ def check_mp3_files(folder_path, summary):
             except Exception as e:
                 print(f"Error processing file '{filename}': {str(e)}")
 
-    print(f"Non-compliant files found: {len(non_compliant_files)}")
-    for filename, missing_tags in non_compliant_files:
-        print(f"\tFile: {filename} is missing {missing_tags}")
+    print(f"Total files checked: {total_files}, "
+          f"Compliant: {total_files - len(non_compliant_files)} "
+          f"Non-compliant: {len(non_compliant_files)}")
+    for non_compliant_file in non_compliant_files:
+        print(f'Non-compliant {non_compliant_file}')
 
     if summary:
-        print(f"Total files checked: {total_files}, "
-              f"Compliant: {total_files - len(non_compliant_files)} "
-              f"Non-compliant: {len(non_compliant_files)}")
-
         print("\nTag Summary:")
         for tag in expected_tags:
             print(f"\n=== {tag}:")
@@ -69,6 +76,34 @@ def check_mp3_files(folder_path, summary):
                     print(f"{value} ({count} file(s))")
 
 
+def export_sermon_tags_to_csv(folder_path):
+    total_files = 0
+    file_name = 'sermons.csv'
+    with open(file_name, 'w') as f:
+        write = csv.writer(f)
+        write.writerow(csv_headers)
+        for filename in os.listdir(folder_path):
+            if filename.endswith('.mp3'):
+                total_files += 1
+                file_path = os.path.join(folder_path, filename)
+                try:
+                    audio = EasyID3(file_path)
+                    keys = audio.keys()
+                    remove_tags = [tag for tag in keys if tag not in expected_tags]
+                    if remove_tags:
+                        for tag in remove_tags:
+                            print(f'Skipping export of tag {tag} from {filename}')
+                            audio.pop(tag)
+                    # Assume the order of the tag values matches the headers - may not always hold
+                    row = audio.values()
+                    row.append(audio.filename)  # full path + filename
+                    row.append(Path(audio.filename).name)  # just the filename
+                    write.writerow(row)
+                except Exception as e:
+                    print(f"Error processing mp3 file for csv export' {filename}': {str(e)}")
+        print(f'Exported {total_files} to {file_name}')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Check MP3 files for expected ID3 tags.')
     parser.add_argument('folder_path', metavar='FOLDER_PATH', type=str, nargs='?',
@@ -77,6 +112,11 @@ if __name__ == '__main__':
                         action='store_true')
     parser.add_argument('-c', '--correct', help='(future) automatically correct missing tags where possible',
                         action='store_true')
+    parser.add_argument('-x', '--export', help='Export csv summary',
+                        action='store_true')
     args = parser.parse_args()
 
-    check_mp3_files(args.folder_path, args.summary)
+    if args.export:
+        export_sermon_tags_to_csv(args.folder_path)
+    else:
+        check_mp3_files_have_tags(args.folder_path, args.summary)
